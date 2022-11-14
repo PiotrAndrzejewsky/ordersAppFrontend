d18df38dc6311cba515b6b3ab8fd5573b2c0398a
PiotrAndrzejewsky <piotrandrzejewski@onet.eu>
2022-11-14 18:21:56 +0100

    feat: add redirect to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
@@ -6,6 +6,10 @@ const routes: Routes = [
   {
     path:"login",
     component:LoginComponent
+  },
+  {
+    path:"**",
+    redirectTo:"login"
   }
 ];
 
