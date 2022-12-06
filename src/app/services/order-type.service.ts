3f7e78dfbb0074c8ae772e9c3992207e835f3df8
PiotrAndrzejewsky <piotrandrzejewski@onet.eu>
2022-12-06 16:56:25 +0100

    feat: create service for order types

---
+++ src/app/services/order-type.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { CookieService } from "ngx-cookie-service";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+import { OrderType } from "../models/order-type.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class OrderTypeService {
+    constructor(
+        private http: HttpClient,
+        private cookieService: CookieService,
+    ) {}
+
+    public headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `${this.cookieService.get(environment.AccessToken)}`
+    });
+
+    getOrderTypesByUserId(): Observable<Array<OrderType>> {
+        let url = environment.baseUrl + environment.getOrderTypesByUserUrl + this.cookieService.get("id")
+        return this.http.get<Array<OrderType>>(url, {headers: this.headers})
+    }
+}
